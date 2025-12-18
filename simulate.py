0da185003dbc93acd8cb5372ac6809f25b2e2b2d
Marat Zakirov <marat61@gmail.com>
2025-12-18 23:22:48 +0300

    euler solution

--- simulate.py
+++ simulate.py
@@ -14,12 +14,10 @@ p_length = 100
 # Initial state (example values)
 # X | dX/dt | T | dT/dt
 X = np.zeros(4)
+X[2] += 0.1
 
-# Matrix transformation from coordinates to display coordinates
-def convert_2_display(X: np.array):
-    Y = np.copy(X)
-    Y[0] += W/2
-    return Y
+def transform(x: int, y: int):
+    return x + W//2, 0.9*H - y
 
 # --- Pygame Initialization ---
 pygame.init()
@@ -34,33 +32,51 @@ class Solution():
         self.l = l
         self.g = 9.8
 
+    def calc_dX(self, X):
+        m, M, l, g = self.m, self.M, self.l, self.g
+        D = lambda x: self.M + self.m*(np.sin(x)**2)
+        # Set u to some non zero value
+        u = 0
+        x, dx, theta, dtheta = X
+        dX = np.zeros(4)
+        dX[0] = dx
+        dX[2] = dtheta
+        dX[1] = (u + m*l*np.sin(X[2])*X[3]**2 - m*g*np.sin(X[2])*np.cos(X[2])) / D(X[2])
+        dX[3] = (-u*np.cos(X[2]) - m*l*np.sin(X[2])*np.cos(X[2])*X[3]**2 + (M+m)*g*np.sin(X[2])) / (l*D(X[2]))
+        return dX
+
     def step(self, X: np.array):
         x, dx, theta, dtheta = X
         L = 0.5*(self.M + self.m)*dx**2 + self.m*self.l*dx*dtheta*np.cos(theta) + 0.5*self.m*(self.l**2)*(dtheta**2) - self.m*self.g*self.l*np.cos(theta)
+        dX = self.calc_dX(X)
+        X = X + dX * dt
         return X
 
 class Stuff():
     def __init__(self):
         self.cart_W = 60
-        self.cart_H = 30
+        self.cart_H = 20
         self.p_length = p_length
 
     # --- Functions for drawing ---
     def draw_cart(self, screen, cart_x, cart_y):
-        cart_rect = pygame.Rect(cart_x - self.cart_W // 2, cart_y - self.cart_H, self.cart_W, self.cart_H)
+        x, y = transform(cart_x, cart_y)
+        pygame.draw.line(screen, (0, 0, 0), (0, H*0.9), (W, H*0.9), 2)
+        cart_rect = pygame.Rect(x - self.cart_W//2, y - self.cart_H, self.cart_W, self.cart_H)
         pygame.draw.rect(screen, (0, 0, 255), cart_rect)
 
     def draw_pendulum(self, screen, cart_x, cart_y, end_x, end_y):
-        pygame.draw.line(screen, (0,0,0), (cart_x, cart_y), (int(end_x), int(end_y)), 5)
-        pygame.draw.circle(screen, (255, 0, 0), (int(end_x), int(end_y)), 15)
+        x, y = transform(cart_x, cart_y)
+        ex, ey = transform(end_x, end_y)
+        pygame.draw.line(screen, (0,0,0), (x, y), (ex, ey), 5)
+        pygame.draw.circle(screen, (255, 0, 0), (ex, ey), 15)
 
     def draw(self, screen, X):
         x, _, theta, _ = X
-        pygame.draw.line(screen, (0, 0, 0), (0, H*0.9), (W, H*0.9), 2)
-        end_x = x + self.p_length * math.sin(theta + np.pi)
-        end_y = H * 0.9 + self.p_length * math.cos(theta + np.pi)
-        self.draw_pendulum(screen, x, H*0.9, end_x, end_y)
-        self.draw_cart(screen, x, H * 0.9)
+        end_x = x + self.p_length * math.sin(theta)
+        end_y = 0 + self.p_length * math.cos(theta)
+        self.draw_pendulum(screen, x, 0, end_x, end_y)
+        self.draw_cart(screen, x, 0)
 
 stuff = Stuff()
 sol = Solution(m, M, p_length)
@@ -78,7 +94,7 @@ while running:
 
     # 3. Drawing
     screen.fill((255, 255, 255))
-    stuff.draw(screen, convert_2_display(X))
+    stuff.draw(screen, X)
     X = sol.step(X)
 
     # 4. Update the display
